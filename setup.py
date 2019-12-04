8cb6180bb496e66dc3235c0e593813bc812bd8ff
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-12-04 23:40:25 +0100

    update README

--- setup.py
+++ setup.py
@@ -4,24 +4,22 @@ from os import path
 
 __version__ = '0.5.0'
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # get the dependencies and installs
+here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
     name='teller',
     version=__version__,
     description='teller',
-    long_description=long_description,
+    long_description="A model-agnostic tool for Machine Learning Explainability",
     url='https://github.com/thierrymoudiki/teller',
     download_url='https://github.com/thierrymoudiki/teller/tarball/' + __version__,
     license='BSD',
@@ -36,6 +34,5 @@ setup(
     author='T. Moudiki',
     install_requires=["numpy >= 1.13.0", "pandas >= 0.25.1", 
                       "scipy >= 0.19.0", "scikit-learn >= 0.18.0"].append(install_requires),
-    dependency_links=dependency_links,
     author_email='thierry.moudiki@gmail.com'
 )
